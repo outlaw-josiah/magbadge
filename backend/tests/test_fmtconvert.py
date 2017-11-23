f46b3e119b5a20f9bbe3b083e06b32792c710382
Tahvohck <Tahvohck@gmail.com>
2017-11-23 09:38:45 -0500

    Cleaned up test_BasicAttendeeToCSV to be more readable with less repetition

--- backend/tests/test_fmtconvert.py
+++ backend/tests/test_fmtconvert.py
@@ -5,7 +5,7 @@ from util		import fmtconvert
 class FmtConversions(unittest.TestCase):
 	dummy_stripped	= dict(
 		name = "Edward Richardson", badge = "RRU-28413", ribbon = "no ribbon",
-		badge_t = "staff", badge_n = 765, hr_total = 30, hr_worked = 0
+		badge_t = "staff", badge_n = 500, hr_total = 30, hr_worked = 0
 	)
 
 	@unittest.expectedFailure
@@ -14,15 +14,13 @@ class FmtConversions(unittest.TestCase):
 
 	def test_BasicAttendeeToCSV(self):
 		epoch = datetime(1970,1,1)
-		with self.subTest():
-			self.assertEqual(
+		compstr = "1970-01-01 00:00:00,staff,500,Edward Richardson,0,30,no ribbon,"
+		with self.subTest():self.assertEqual(
 				fmtconvert.BasicAttendeeToCSV(epoch, self.dummy_stripped),
-				"1970-01-01 00:00:00,staff,765,Edward Richardson,0,30,no ribbon,")
-		with self.subTest("Input Validation: Wrong type"):
-			self.assertEqual(
-				fmtconvert.BasicAttendeeToCSV(epoch, self.dummy_stripped, 3),
-				"1970-01-01 00:00:00,staff,765,Edward Richardson,0,30,no ribbon,")
-		with self.subTest("Input Validation"):
-			self.assertEqual(
+				compstr)
+		with self.subTest("Input Validation: Wrong type"):self.assertEqual(
+				fmtconvert.BasicAttendeeToCSV(epoch, self.dummy_stripped,3),
+				compstr)
+		with self.subTest("Input Validation"):self.assertEqual(
 				fmtconvert.BasicAttendeeToCSV(epoch, self.dummy_stripped, False),
-				"1970-01-01 00:00:00,staff,765,Edward Richardson,0,30,no ribbon,False")
+				compstr + "False")
