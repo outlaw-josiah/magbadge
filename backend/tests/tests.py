ae370f09ee6fa28b19daa050134ee6d3a38450f8
Tahvohck <Tahvohck@gmail.com>
2018-01-05 18:46:07 -0500

    Remove unit test for the code that was deleted in commit 5aba84 (5aba8442d59805c075699ba0b695611ddd20bd0f)

--- backend/tests/tests.py
+++ backend/tests/tests.py
@@ -9,7 +9,6 @@ import datetime
 from unittest.mock	import MagicMock
 from json			import loads
 from datetime		import datetime
-from util			import fmtconvert
 from testfixtures	import log_capture
 from argparse		import Namespace
 
@@ -19,32 +18,6 @@ class FmtConversions(unittest.TestCase):
 		name="Edward Richardson", badge="RRU-28413", ribbon="no ribbon",
 		badge_t="staff", badge_n=500, hr_total=30, hr_worked=0
 	)
-
-	@log_capture(level=logging.ERROR)
-	def test_magapiToBasicAttendee(self, capture):
-		with self.subTest("Input Validation"):
-			self.assertEqual(fmtconvert.magapiToBasicAttendee("Bad input"), {})
-		with self.subTest("Input Validation 2"):
-			self.assertEqual(fmtconvert.magapiToBasicAttendee(
-				"Bad input long text 12345678901234567890"), {})
-		capture.check(
-			("util.fmtconvert", "ERROR",
-			"Malformed data in conversion: Bad input"),
-			("util.fmtconvert", "ERROR",
-			"Malformed data in conversion: Bad input long text 1234567890..."))
-
-	def test_BasicAttendeeToCSV(self):
-		epoch = datetime(1970, 1, 1)
-		compstr = "1970-01-01 00:00:00,staff,500,Edward Richardson,0,30,no ribbon,"
-		with self.subTest(): self.assertEqual(
-			fmtconvert.BasicAttendeeToCSV(epoch, self.dummy_stripped),
-			compstr)
-		with self.subTest("Input Validation: Wrong type"): self.assertEqual(
-			fmtconvert.BasicAttendeeToCSV(epoch, self.dummy_stripped, 3),
-			compstr)
-		with self.subTest("Input Validation"): self.assertEqual(
-			fmtconvert.BasicAttendeeToCSV(epoch, self.dummy_stripped, False),
-			compstr + "False")
 
 	# Doing this test just in case. Should never fail.
 	def test_datetime_strftime(self):
